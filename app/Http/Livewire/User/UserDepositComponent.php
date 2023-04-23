cd359227a2281647b0866bc843a99232476836a8
ked-k <kedkayz@gmail.com>
2023-04-23 18:44:31 +0300

    initials livewire installation

---
+++ app/Http/Livewire/User/UserDepositComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class UserDepositComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.user-deposit-component');
+    }
+}
